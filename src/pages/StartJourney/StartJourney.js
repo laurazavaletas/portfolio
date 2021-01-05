57805837cca88ca245d42ec85b5c506c078093b6
DouglasHdezT <00004516@uca.edu.sv>
2021-01-05 15:47:25 -0600

    Update: Little things

--- src/pages/StartJourney/StartJourney.js
+++ src/pages/StartJourney/StartJourney.js
@@ -4,11 +4,23 @@ import classes from './StartJourney.module.css';
 import bg from '@internal/assets/images/backgrounds/startjourney.jpg';
 
 import NavPage from '@internal/components/NavPage/NavPage';
+import MainFooter from '@internal/components/MainFooter/MainFooter';
 
 const StartJourney = props => {
     return (
         <NavPage bgimage={bg} about>
-
+            <main className={classes.container}>
+                <div className={classes.title}>
+                    <h1>Start</h1>
+                    <h1>Journey</h1>
+                </div>
+    
+                <a className={ classes.email } href="mailto:laumazasa@gmail.com">
+                    laumazasa@gmail.com
+                </a>
+    
+                <MainFooter/>
+            </main>
         </NavPage>
     );
 }
